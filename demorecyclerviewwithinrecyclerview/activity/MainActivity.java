39633a74ff0b0746256118462e852459b7039fde
ris269 <1242032@student.hcmus.edu.vn>
2017-12-22 14:46:26 +0700

    init project

---
+++ demorecyclerviewwithinrecyclerview/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ris269.demorecyclerviewwithinrecyclerview.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ris269.demorecyclerviewwithinrecyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
